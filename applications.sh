034a3d65b9707e8971a33e46d7ef3dcee9705784
Tim Kendrick <timkendrick@gmail.com>
2016-11-16 21:41:12 +0000

    Build AppleScript applications in $TMPDIR

--- applications.sh
+++ applications.sh
@@ -1,13 +1,13 @@
 set -e
 
-mkdir -p ./.tmp/Applications
+mkdir -p $TMPDIR/Applications
 
-osacompile -o ./.tmp/Applications/Launch\ Server\ Here.app ./Applications/Launch\ Server\ Here/main.applescript
-cp ./Applications/Launch\ Server\ Here/icon.icns ./.tmp/Applications/Launch\ Server\ Here.app/Contents/Resources/applet.icns
+osacompile -o $TMPDIR/Applications/Launch\ Server\ Here.app ./Applications/Launch\ Server\ Here/main.applescript
+cp ./Applications/Launch\ Server\ Here/icon.icns $TMPDIR/Applications/Launch\ Server\ Here.app/Contents/Resources/applet.icns
 
-osacompile -o ./.tmp/Applications/Open\ Terminal\ Here.app ./Applications/Open\ Terminal\ Here/main.applescript
-cp ./Applications/Open\ Terminal\ Here/icon.icns ./.tmp/Applications/Open\ Terminal\ Here.app/Contents/Resources/applet.icns
+osacompile -o $TMPDIR/Applications/Open\ Terminal\ Here.app ./Applications/Open\ Terminal\ Here/main.applescript
+cp ./Applications/Open\ Terminal\ Here/icon.icns $TMPDIR/Applications/Open\ Terminal\ Here.app/Contents/Resources/applet.icns
 
-cp -r ./.tmp/Applications/* /Applications/
+cp -r $TMPDIR/Applications/* /Applications/
 
-rm -rf ./tmp/Applications
+rm -rf $TMPDIR/Applications

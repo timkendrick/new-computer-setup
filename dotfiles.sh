f84db054b0b45fa146c3ce8565e917fa8ae1b644
Tim Kendrick <timkendrick@gmail.com>
2016-11-17 21:25:53 +0000

    Add npm package configuration defaults

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,3 +7,8 @@ echo "Setting Git configuration..."
 cp ./dotfiles/.gitignore  ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
 git config --global credential.helper osxkeychain
+
+echo "Setting npm configuration..."
+npm config set init-author-name Tim Kendrick
+npm config set init-author-email timkendrick@gmail.com
+npm config set init-version 0.0.1

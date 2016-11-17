dabf3d976c491c3390328b2103a071128cf23e8e
Tim Kendrick <timkendrick@gmail.com>
2016-11-17 00:46:40 +0000

    Prevent buggy Caps Lock disabling

--- Preferences/Seil.sh
+++ Preferences/Seil.sh
@@ -1,6 +1,6 @@
 # Disable Caps Lock modifier key
-KEYBOARD_ID="$(ioreg -n IOHIDKeyboard -r | grep -e \"VendorID\" | sed 's/^.*\"VendorID\" = \([0-9]*\)/\1/')-$(ioreg -n IOHIDKeyboard -r | grep -e \"ProductID\" | sed 's/^.*\"ProductID\" = \([0-9]*\)/\1/')-0"
-defaults -currentHost write NSGlobalDomain "com.apple.keyboard.modifiermapping.$KEYBOARD_ID" '( { HIDKeyboardModifierMappingSrc = 0; HIDKeyboardModifierMappingDst = -1; } )'
+# KEYBOARD_ID="$(ioreg -n IOHIDKeyboard -r | grep -e \"VendorID\" | sed 's/^.*\"VendorID\" = \([0-9]*\)/\1/')-$(ioreg -n IOHIDKeyboard -r | grep -e \"ProductID\" | sed 's/^.*\"ProductID\" = \([0-9]*\)/\1/')-0"
+# defaults -currentHost write NSGlobalDomain "com.apple.keyboard.modifiermapping.$KEYBOARD_ID" '( { HIDKeyboardModifierMappingSrc = 0; HIDKeyboardModifierMappingDst = -1; } )'
 
 # Remap Caps Lock modifier key to PC application key
 defaults write org.pqrs.Seil sysctl '{ "enable_capslock" = 1; "keycode_capslock" = 110; }'
